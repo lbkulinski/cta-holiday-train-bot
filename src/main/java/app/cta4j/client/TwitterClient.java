af37bf2211806458fbea6588c857e9def3e0195e
Logan Kulinski <rashes_lineage02@icloud.com>
2023-11-18 13:51:48 -0600

    Twitter updates

--- src/main/java/app/cta4j/client/TwitterClient.java
+++ src/main/java/app/cta4j/client/TwitterClient.java
@@ -3,8 +3,8 @@ package app.cta4j.client;
 import com.github.scribejava.apis.TwitterApi;
 import com.github.scribejava.core.builder.ServiceBuilder;
 import com.github.scribejava.core.model.OAuth1AccessToken;
-import com.github.scribejava.core.model.OAuth1RequestToken;
 import com.github.scribejava.core.model.OAuthRequest;
+import com.github.scribejava.core.model.Response;
 import com.github.scribejava.core.model.Verb;
 import com.github.scribejava.core.oauth.OAuth10aService;
 import com.rollbar.notifier.Rollbar;
@@ -12,11 +12,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.util.Objects;
-import java.util.Scanner;
 import java.util.concurrent.ExecutionException;
 
 @Component
@@ -35,51 +35,25 @@ public final class TwitterClient {
 
     @Autowired
     public TwitterClient(Rollbar rollbar, @Value("${twitter.consumer-key}") String consumerKey,
-        @Value("${twitter.consumer-secret}") String consumerSecret) {
+        @Value("${twitter.consumer-secret}") String consumerSecret,
+        @Value("${twitter.access-token}") String accessToken,
+        @Value("${twitter.access-token-secret}") String accessTokenSecret) {
         this.rollbar = Objects.requireNonNull(rollbar);
 
         Objects.requireNonNull(consumerKey);
 
         Objects.requireNonNull(consumerSecret);
 
+        Objects.requireNonNull(accessToken);
+
+        Objects.requireNonNull(accessTokenSecret);
+
         ServiceBuilder serviceBuilder = new ServiceBuilder(consumerKey);
 
         this.service = serviceBuilder.apiSecret(consumerSecret)
                                      .build(TwitterApi.instance());
 
-        this.accessToken = TwitterClient.getAccessToken(this.service);
-    }
-
-    private static OAuth1AccessToken getAccessToken(OAuth10aService service) {
-        Objects.requireNonNull(service);
-
-        OAuth1RequestToken requestToken;
-
-        try {
-            requestToken = service.getRequestToken();
-        } catch (IOException | InterruptedException | ExecutionException e) {
-            throw new RuntimeException(e);
-        }
-
-        String authorizationUrl = service.getAuthorizationUrl(requestToken);
-
-        System.out.println(authorizationUrl);
-
-        System.out.print(">> ");
-
-        Scanner scanner = new Scanner(System.in);
-
-        String verifier = scanner.nextLine();
-
-        OAuth1AccessToken accessToken;
-
-        try {
-            accessToken = service.getAccessToken(requestToken, verifier);
-        } catch (IOException | InterruptedException | ExecutionException e) {
-            throw new RuntimeException(e);
-        }
-
-        return accessToken;
+        this.accessToken = new OAuth1AccessToken(accessToken, accessTokenSecret);
     }
 
     public void createTweet(String text) {
@@ -98,14 +72,30 @@ public final class TwitterClient {
 
         this.service.signRequest(this.accessToken, request);
 
+        Response response;
+
         try {
-            this.service.execute(request);
+            response = this.service.execute(request);
         } catch (IOException | ExecutionException | InterruptedException e) {
             String message = e.getMessage();
 
             TwitterClient.LOGGER.error(message, e);
 
             this.rollbar.error(e);
+
+            return;
+        }
+
+        int code = response.getCode();
+
+        int created = HttpStatus.CREATED.value();
+
+        if (code != created) {
+            String message = "Failed to create tweet";
+
+            TwitterClient.LOGGER.error(message);
+
+            this.rollbar.error(message);
         }
     }
 }
