fb6236e72e1b2668b384b2347f4994c2b3c252fe
Logan Kulinski <rashes_lineage02@icloud.com>
2024-11-23 23:37:52 -0600

    Web server update

--- src/main/java/app/cta4j/Application.java
+++ src/main/java/app/cta4j/Application.java
@@ -1,7 +1,6 @@
 package app.cta4j;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -9,10 +8,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class Application {
     public static void main(String[] args) {
-        SpringApplication application = new SpringApplication(Application.class);
-
-        application.setWebApplicationType(WebApplicationType.NONE);
-
-        application.run(args);
+        SpringApplication.run(Application.class, args);
     }
 }
